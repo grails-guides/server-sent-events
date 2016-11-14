e6325c683145b0661d92af3948ab1df7aeeb41a7
graemerocher <graeme.rocher@gmail.com>
2016-11-14 09:58:42 +0100

    Share travis build

--- travis-build.sh
+++ travis-build.sh
@@ -3,36 +3,9 @@ set -e
 
 export EXIT_STATUS=0
 
-./gradlew check || EXIT_STATUS=$?
+curl -O https://raw.githubusercontent.com/grails/grails-guides/a2d214da3d37c72c93b7b53c8f04bb56fe04cd0f/travis/build-guide
+chmod 777 build-guide
 
-if [[ $EXIT_STATUS -eq 0 ]]; then
-    ./gradlew publishGuide || EXIT_STATUS=$?
-fi
-
-if [[ $EXIT_STATUS -eq 0 && $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-    echo "Publishing Documentation"
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
-    git config --global credential.helper "store --file=~/.git-credentials"
-    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
-
-    git clone https://${GH_TOKEN}@github.com/grails/grails-guides.git -b gh-pages gh-pages --single-branch > /dev/null
-    
-    ./gradlew updateGuidesJson
-
-    cd gh-pages
-
-        
-    # If this is the master branch then update the snapshot
-    mkdir -p server-sent-events
-    cp -r ../build/docs/. ./server-sent-events/
-
-    git add .   
-
-    git commit -a -m "Publishing Grails Guide for Travis Build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-    git push origin HEAD
-    cd ..
-    rm -rf gh-pages
-fi    
+./build-guide || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
