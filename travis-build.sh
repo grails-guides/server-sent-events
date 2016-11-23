9d216744fba56cdfe037892a74f303885d13b99d
graemerocher <graeme.rocher@gmail.com>
2016-11-23 14:45:35 +0100

    Use master of grails-guides

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@ set -e
 
 export EXIT_STATUS=0
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/a314e30c2da9779ee623dc44ad579f818b5c5929/travis/build-guide
+curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
 chmod 777 build-guide
 
 ./build-guide || EXIT_STATUS=$?
